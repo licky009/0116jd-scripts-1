dcf81c0c609369451b44f3c4f1f24c618f4d7718
fm1223 <xiaochen_vip8@outlook.com>
2020-11-28 18:53:53 +0800

    Update jdDreamFactoryShareCodes.js

--- jdDreamFactoryShareCodes.js
+++ jdDreamFactoryShareCodes.js
@@ -8,8 +8,8 @@
 // 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
 let shareCodes = [
-  '2Ulj8cF7fqhTpQZy8nPu3Q==',//账号一的好友shareCode,不同好友中间用@符号隔开
-  'PC-yzyURlilMIiB2ftCAVw==',//账号二的好友shareCode，不同好友中间用@符号隔开
+  '2Ulj8cF7fqhTpQZy8nPu3Q==@2Ulj8cF7fqhTpQZy8nPu3Q==@NMepycxM402Mdi8Uf1REYQ==@Wl_FnG0MmzVm8AwKLpsQNQ==@CxP1dZf-BY4_ih33ts7zrQ==@J6vJoqImrQn86hBCJ7BC8g==@UCiA0kajv8rhFzaCs-YJ4Q==@bzSm8hpvUZ6ZPAdqpQ9V9g==@al5U5qV2hz3YmqYMw3iJNw==@e83NYFq0IWz4vUp6WNQ2sA==@ZK_vajsXsogn_C4Vcsf2GQ==@PC-yzyURlilMIiB2ftCAVw==@TW2DYkF2JwlYeeyAj7KIPA==@yIUyUYm1heMC3o_ERYTVrQ==@LIvLd05LJqjeDeEDzS2IKQ==',//账号一的好友shareCode,不同好友中间用@符号隔开
+  'PC-yzyURlilMIiB2ftCAVw==@2Ulj8cF7fqhTpQZy8nPu3Q==@NMepycxM402Mdi8Uf1REYQ==@Wl_FnG0MmzVm8AwKLpsQNQ==@CxP1dZf-BY4_ih33ts7zrQ==@J6vJoqImrQn86hBCJ7BC8g==@UCiA0kajv8rhFzaCs-YJ4Q==@bzSm8hpvUZ6ZPAdqpQ9V9g==@al5U5qV2hz3YmqYMw3iJNw==@e83NYFq0IWz4vUp6WNQ2sA==@ZK_vajsXsogn_C4Vcsf2GQ==@PC-yzyURlilMIiB2ftCAVw==@TW2DYkF2JwlYeeyAj7KIPA==@yIUyUYm1heMC3o_ERYTVrQ==@LIvLd05LJqjeDeEDzS2IKQ==',//账号二的好友shareCode，不同好友中间用@符号隔开
 ]
 // 判断github action里面是否有东东萌宠互助码
 if (process.env.DREAM_FACTORY_SHARE_CODES) {

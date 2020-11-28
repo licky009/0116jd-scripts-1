db9e225d314b89622614521ea910f15985212a81
fm1223 <xiaochen_vip8@outlook.com>
2020-11-28 18:15:16 +0800

    Update jdFactoryShareCodes.js

--- jdFactoryShareCodes.js
+++ jdFactoryShareCodes.js
@@ -1,5 +1,5 @@
 /*
-京东种豆得豆互助码
+东东工厂互助码
 此文件为Node.js专用。其他用户请忽略
 支持京东N个账号
  */
@@ -10,22 +10,22 @@
 let shareCodes = [
     'P04z54XCjVWnYaS5m9cZ2Ws2i0bnN5PfNWNqmI',//账号一的好友shareCode,不同好友中间用@符号隔开
     'P04z54XCjVWnYaS5n1RWT-rhSkU-iEooQ',//账号二的好友shareCode，不同好友中间用@符号隔开
-  ]
-  // 判断github action里面是否有东东萌宠互助码
-  if (process.env.DDFACTORY_SHARECODES) {
-    if (process.env.DDFACTORY_SHARECODES.indexOf('&') > -1) {
-      console.log(`您的互助码选择的是用&隔开\n`)
-      shareCodes = process.env.DDFACTORY_SHARECODES.split('&');
-    } else if (process.env.DDFACTORY_SHARECODES.indexOf('\n') > -1) {
-      console.log(`您的互助码选择的是用换行隔开\n`)
-      shareCodes = process.env.DDFACTORY_SHARECODES.split('\n');
-    } else {
-      shareCodes = process.env.DDFACTORY_SHARECODES.split();
-    }
-  } else if (process.env.JD_COOKIE) {
-    console.log(`由于您secret里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
+]
+// 判断github action里面是否有东东工厂互助码
+if (process.env.DDFACTORY_SHARECODES) {
+  if (process.env.DDFACTORY_SHARECODES.indexOf('&') > -1) {
+    console.log(`您的互助码选择的是用&隔开\n`)
+    shareCodes = process.env.DDFACTORY_SHARECODES.split('&');
+  } else if (process.env.DDFACTORY_SHARECODES.indexOf('\n') > -1) {
+    console.log(`您的互助码选择的是用换行隔开\n`)
+    shareCodes = process.env.DDFACTORY_SHARECODES.split('\n');
+  } else {
+    shareCodes = process.env.DDFACTORY_SHARECODES.split();
   }
-  for (let i = 0; i < shareCodes.length; i++) {
-    const index = (i + 1 === 1) ? '' : (i + 1);
-    exports['shareCodes' + index] = shareCodes[i];
-  }
+} else if (process.env.DDFACTORY_SHARECODES) {
+  console.log(`由于您secret里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
+}
+for (let i = 0; i < shareCodes.length; i++) {
+  const index = (i + 1 === 1) ? '' : (i + 1);
+  exports['shareCodes' + index] = shareCodes[i];
+}
